a14c0bd822bd1127fe2166a4726dc152972ee823
TheDane13 <dharris131313@gmail.com>
2026-03-27 01:51:43 -0400

    Add service worker for offline support and caching

---
+++ public/sw.js
@@ -0,0 +1,22 @@
+// This is the service worker for offline support and caching
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('my-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/styles.css',
+        '/script.js',
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
